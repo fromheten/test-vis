6c5438538a59eb14cdabe7c17378f4b52cc2d31e
Martin Josefsson <hello@martinjosefsson.com>
2017-11-20 09:16:50 +0100

    Add platoPaths for finding all paths in an expression leading to a plato call

--- test/js/lib/expand_tests.js
+++ test/js/lib/expand_tests.js
@@ -1,6 +1,6 @@
 const test = require("tape")
-const { equals } = require("ramda")
-const { expand, paths, oneLevelPaths } = require("../../../lib/expand")
+const { equals, not } = require("ramda")
+const { expand, paths, isPlato, platoPaths } = require("../../../lib/expand")
 
 test("expanding an expression with no plato calls returns like identity", t => {
   t.deepLooseEqual(expand(["cons", ["quote", "a"], ["quote", "b"]]), [
@@ -22,5 +22,47 @@ test("path gen", t => {
     [1, 0, 0],
     [2]
   ])
+  t.end()
+})
+
+test("finding a plato expression", t => {
+  t.assert(
+    isPlato(["plato", "plato:0:K15fdTwF0lDGzG5rM4lx3lLKCzy1+QTtyeOwTUd+3vY="])
+  )
+  t.assert(
+    not(
+      isPlato([
+        "not plato",
+        "plato:0:K15fdTwF0lDGzG5rM4lx3lLKCzy1+QTtyeOwTUd+3vY="
+      ])
+    )
+  )
+  t.end()
+})
+
+test("finding plato expressions in a tree", t => {
+  t.deepLooseEqual(
+    platoPaths([
+      "plato",
+      "plato:0:K15fdTwF0lDGzG5rM4lx3lLKCzy1+QTtyeOwTUd+3vY="
+    ]),
+    [[]],
+    "An empty array means the whole thing is a plato expression"
+  )
+  t.deepLooseEqual(
+    platoPaths([
+      ["plato", "plato:0:K15fdTwF0lDGzG5rM4lx3lLKCzy1+QTtyeOwTUd+3vY="],
+      "hallå"
+    ]),
+    [[0]]
+  )
+  t.deepLooseEqual(
+    platoPaths([
+      [[["plato", "plato:0:K15fdTwF0lDGzG5rM4lx3lLKCzy1+QTtyeOwTUd+3vY="]]],
+      ["whatever", "something"]
+    ]),
+    [[0, 0, 0]]
+  )
+  t.deepLooseEqual(platoPaths([["nix!", "no expansion here"], "hallå"]), [])
   t.end()
 })
